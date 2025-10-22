d5e1be712e9a692a871218dc4f968abac93501ff
betstick <61669265+betstick@users.noreply.github.com>
2025-10-21 21:10:03 -0400

    Update bnd3.h

--- src/binder/bnd3.h
+++ src/binder/bnd3.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "binder.h"
+#include <stdexcept>
 
 class bnd3_t : public binder_t
 {
@@ -10,8 +11,45 @@ class bnd3_t : public binder_t
 	bool big_endian = false;
 	bool bit_big_endian = false;
 	
+	public:
 	static bnd3_t* read(UMEM* mem)
 	{
+		struct Header
+		{
+			char magic[4];
+			char version[8];
+			uint8_t rawFormat;
+			uint8_t bigEndian;
+			uint8_t bitBigEndian;
+			char unk0F;
+			int32_t fileCount;
+			int32_t fileHeadersEnd;
+			int32_t unk18;
+			int32_t unk1C; //assert 0
+		};
+
+		/*{
+			Header h;
+			mem->read(&h,sizeof(Header),1);
+			std::string magic_str = "";
+			for(i32 i = 0; i < 4; i++)
+				magic_str.push_back(h.magic[i]);
+			printf("magic: %s\n",magic_str.c_str());
+			std::string ver_str = "";
+			for(i32 i = 0; i < 8; i++)
+				ver_str.push_back(h.version[i]);
+			printf("version: %s\n",ver_str.c_str());
+			printf("rawFormat: %u\n",h.rawFormat);
+			printf("bigEndian: %u\n",h.bigEndian);
+			printf("bitBigEndian: %u\n",h.bitBigEndian);
+			printf("unk0F: %u\n",h.unk0F);
+			printf("file_count: %i\n",h.fileCount);
+			printf("unk18: %i\n",h.unk18);
+			printf("unk1C: %i\n",h.unk1C);
+			printf("--------------\n");
+			mem->seek(-sizeof(Header),SEEK_CUR);
+		}*/
+
 		bnd3_t* bnd = new bnd3_t();
 
 		auto assert_fn = [&](bool b, std::string err = "")
@@ -24,29 +62,41 @@ class bnd3_t : public binder_t
 			return b;
 		};
 
-		std::string magic = "\0\0\0\0";
-		mem->read(magic.data(),sizeof(char),4);
-		if(!assert_fn(same_str(magic,"BND3"),"got magic "+magic))
-			return nullptr;
+		{
+			char magic[4];
+			mem->read(magic,sizeof(char),4);
+			std::string magic_str = "";
+			for(i32 i = 0; i < 4; i++)
+				magic_str.push_back(magic[i]);
+			//printf("magic: %s\n",magic_str.c_str());
+			if(!assert_fn(same_str(magic,"BND3"),"got magic "+magic_str))
+			{
+				throw std::runtime_error("Ah\n");
+				return nullptr;
+			}
+		}
 
 		bnd->version = "00000000";
 		mem->read(bnd->version.data(),sizeof(char),8);
+		//printf("version: %s\n",bnd->version.c_str());
 
-		mem->step_in(0xE)->read(&bnd->bit_big_endian,sizeof(bool),1);
-		mem->step_out();
+		mem->seek(2,SEEK_CUR);
+		bnd->bit_big_endian = mem->read_u8();
+		mem->seek(-3,SEEK_CUR);
 
 		bnd->format = mem->read_format(bnd->bit_big_endian);
-		bnd->big_endian = mem->read_u8();
-		if(!assert_fn(mem->read_u8() != bnd->bit_big_endian))
-			return nullptr;
-		if(!assert_fn(mem->assert_u8(0),"padding 0"))
-			return nullptr;
 
+		bnd->big_endian = mem->read_u8();
+		mem->assert_u8(bnd->bit_big_endian);
+		mem->assert_u8(0);
 		mem->big_endian() = bnd->big_endian || (bnd->format & format_e::fmt_big_endian);
 
 		i32 file_count = mem->read_i32();
-		mem->read_i32();
+		i32 file_header_end = mem->read_i32();
 		bnd->unk18 = mem->read_i32(); //TODO: assert a value for this
+		i32 unk1C = mem->read_i32(); //TODO: assert a value for this
+
+		//printf("file_count: %i\n",file_count);
 
 		for(i32 i = 0; i < file_count; i++)
 			bnd->file_headers.push_back(file_header_t::read_bnd3_fh(
